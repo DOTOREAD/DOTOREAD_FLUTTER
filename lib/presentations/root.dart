ad755434c8d09581e22539f6eda8f4a3e205da4e
usbinn <kidbean9999@gmail.com>
2024-10-07 20:51:07 +0900

    style: bottomnavbar 아이콘 추가

--- lib/presentations/root.dart
+++ lib/presentations/root.dart
@@ -1,7 +1,9 @@
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:dotoread_app/presentations/screens/bookmark_screen.dart';
+import 'package:dotoread_app/presentations/screens/share_screen.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import '../controllers/root_controller.dart';
 import '../core/constants/theme.dart';
@@ -17,15 +19,25 @@ class Root extends StatelessWidget {
   final List<Widget> pages = [
     const HomeScreen(),
     const BookmarkScreen(),
+    const ShareScreen(),
     const DotoriScreen(),
     const SettingScreen(),
   ];
 
-  final List<IconData> iconList = [
-    Icons.home_rounded,
-    Icons.bookmark,
-    Icons.local_fire_department,
-    Icons.person,
+  final List<String> iconList = [
+    'assets/icons/Home.svg',
+    'assets/icons/BookMark.svg',
+    'assets/icons/Share.svg',
+    'assets/icons/Mission.svg',
+    'assets/icons/MyPage.svg',
+  ];
+
+  final List<String> iconClickedList = [
+    'assets/icons/HomeClicked.svg',
+    'assets/icons/BookMarkClicked.svg',
+    'assets/icons/ShareClicked.svg',
+    'assets/icons/MissionClicked.svg',
+    'assets/icons/MyPageClicked.svg',
   ];
 
   Root({super.key});
@@ -49,16 +61,47 @@ class Root extends StatelessWidget {
               },
               child: const Icon(Icons.add),
             ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Obx(() => AnimatedBottomNavigationBar(
-            icons: iconList,
+      bottomNavigationBar: Obx(
+        () => Container(
+          margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          decoration: BoxDecoration(
+            color: AppTheme.gray1,
+            borderRadius: BorderRadius.circular(30),
+            boxShadow: const [
+              BoxShadow(
+                color: AppTheme.articleBox,
+                blurRadius: 30,
+                offset: Offset(0, 5),
+              ),
+            ],
+            border: Border.all(color: AppTheme.gray1, width: 1),
+          ),
+          child: AnimatedBottomNavigationBar.builder(
+            itemCount: iconList.length,
+            tabBuilder: (int index, bool isActive) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: isActive
+                    ? SvgPicture.asset(
+                        iconClickedList[index],
+                        width: 24,
+                        height: 24,
+                      )
+                    : SvgPicture.asset(
+                        iconList[index],
+                        width: 24,
+                        height: 24,
+                      ),
+              );
+            },
             activeIndex: _controller.bottomNavIndex.value,
             onTap: (index) => _controller.updateIndex(index),
-            backgroundColor: AppTheme.gray1,
-            inactiveColor: AppTheme.white1,
-            activeColor: AppTheme.orange1,
-            gapLocation: GapLocation.center,
-          )),
+            backgroundColor: Colors.transparent,
+            gapLocation: GapLocation.none,
+          ),
+        ),
+      ),
     );
   }
 
--- lib/presentations/widgets/store_list.dart
+++ lib/presentations/widgets/store_list.dart
@@ -9,14 +9,16 @@ class StoreList extends StatelessWidget {
   final List<StoreModel> storeList = [
     const StoreModel(
       id: 1,
-      imageUrl: 'https://via.placeholder.com/200',
+      imageUrl:
+          'https://image.oliveyoung.co.kr/uploads/images/goods/400/10/0000/0020/A00000020787602ko.jpg?l=ko',
       name: 'World Wide Fund for Nature',
       content:
           'WWF는 세계적인 비영리 환경보전기관으로, 세계 100여 개국에서 글로벌 네트워크를 구축해 500만 명이상의 후원자들과 함께 활발히 활동하고 있습니다.',
     ),
     const StoreModel(
       id: 2,
-      imageUrl: 'https://via.placeholder.com/200',
+      imageUrl:
+          'https://image.oliveyoung.co.kr/uploads/images/goods/400/10/0000/0020/A00000020787602ko.jpg?l=ko',
       name: '동물권 행동 카라',
       content:
           '당신의 후원으로 동물을 구합니다. 구조와 입양, 정책연구와 법제정, 생명권 교육과 캠페인으로 우리사회의 동물권을 확장합니다. 동물권행동 카라는 정기적인 외부 회계감사와 매월 재정보호로 후원금을 투명하게 운용합니다.',
