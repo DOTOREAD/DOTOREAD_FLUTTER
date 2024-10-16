a93fb7ba2bfa94a4126da21715b0ff1b61e76b82
usbinn <kidbean9999@gmail.com>
2024-10-16 21:27:09 +0900

    feat : 구글 로그인 테스트

--- lib/presentations/screens/share_screen.dart
+++ lib/presentations/screens/share_screen.dart
@@ -1,10 +1,48 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ShareScreen extends StatelessWidget {
   const ShareScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            await _googleLogin(context);
+          },
+          child: const Text(
+            'Google 로그인하기',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _googleLogin(BuildContext context) async {
+    try {
+      final GoogleSignIn googleSignIn = GoogleSignIn(
+        scopes: ['email', 'profile', 'openid'],
+      );
+
+      final GoogleSignInAccount? account = await googleSignIn.signIn();
+
+      if (account != null) {
+        log('Display Name: ${account.displayName}');
+        log('Email: ${account.email}');
+        log('ID: ${account.id}');
+
+        final GoogleSignInAuthentication auth = await account.authentication;
+
+        log('Access Token: ${auth.accessToken}');
+        log('ID Token: ${auth.idToken}');
+        log('Server Auth Code: ${auth.serverAuthCode}');
+      }
+    } catch (error) {
+      log('$error');
+    }
   }
 }
